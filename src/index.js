68c3feb2657fd6cfa3ceb2007935b85904a982c4
Shota Nishiyama <47414233+nissy-shota@users.noreply.github.com>
2022-06-11 23:30:22 +0900

    create event is onclickadd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //create div tag
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // create li tag
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // create complete button tag
+  const completeButton = document.createElement("button");
+  completeButton.innerText("complete");
+
+  // create delete button tag
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText("delete");
+
+  // set div tag of child element li tag.
+  div.appendChild(li);
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
